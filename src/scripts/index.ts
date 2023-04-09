d9ab9208c704d04e445a74be13c81c8d558b93e2
DaveP80 <davidpaq1@gmail.com>
2023-04-08 21:39:45 -0400

    refactor

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -11,9 +11,6 @@ document.querySelector<HTMLDivElement>('#app')!.innerHTML = `
     <a href="https://www.typescriptlang.org/" target="_blank">
       <img src="${typescriptLogo}" class="logo vanilla" alt="TypeScript logo" />
     </a>
-    <h1>Vite + TypeScript</h1>
-    <h2>${import.meta.env.EXAMPLE}</h2>
-    <h2>${import.meta.env.TWO}</h2>
     <div class="card">
       <button id="counter" type="button"></button>
     </div>
